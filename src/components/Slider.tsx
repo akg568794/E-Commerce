05bd2e80fc3317ff5e1d503262baf707a42b7fca
abhay gupta <akg568794@gmail.com>
2024-07-04 19:37:59 +0530

    updated the margin of the sliders's description

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -47,7 +47,7 @@ const Slider = () => {
                 <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
                     {/* TEXT CONTAINER */}
                     <div className=" h-1/3 sm:h-1/2  xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
-                        <h2 className="text:xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
+                        <h2 className="mt-10 text:xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
                         <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
                         <Link href={slide.url}><button className="rounded-md bg-black text-white py-3 px-4">Shop Now</button></Link>
                     </div>
